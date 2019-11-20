98a7556c53b078cec2766ae50fb64b94255a0a0d
TitamSeptian <Itsmystyles9889@gmail.com>
2019-11-20 12:05:01 +0700

    fix master.blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    @stack('js')
 </head>
 
 <body id="page-top">
@@ -51,23 +52,6 @@
     </a>
     <!-- Logout Modal-->
     @include('layouts.modal._logout')
-    {{-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div> --}}
     @include('layouts.modal._modal')
     <!-- Bootstrap core JavaScript-->
     <script src="{{asset('assets/jquery/jquery.min.js')}}"></script>
